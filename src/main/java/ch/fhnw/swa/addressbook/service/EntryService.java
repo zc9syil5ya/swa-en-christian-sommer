3aac4f26c627288f4080763f4e0b449438c6dda5
zc9syil5ya <chregi.sommer@gmail.com>
2020-11-25 21:06:14 +0100

    Create function getAllEntrys()
    Function gets all address book entrys from the database.

--- src/main/java/ch/fhnw/swa/addressbook/service/EntryService.java
+++ src/main/java/ch/fhnw/swa/addressbook/service/EntryService.java
@@ -1,15 +1,24 @@
 package ch.fhnw.swa.addressbook.service;
 
 import ch.fhnw.swa.addressbook.model.Entry;
+import ch.fhnw.swa.addressbook.repository.EntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EntryService {
+    @Autowired
+    EntryRepository repo;
     public List<Entry> getAllEntrys() {
-        return null;
+        List<Entry> employeeList = repo.findAll();
+        if(employeeList.size() > 0) {
+            return employeeList;
+        } else {
+            return new ArrayList<Entry>();
+        }
     }
 
     public Entry getEntryById(Long id) {
