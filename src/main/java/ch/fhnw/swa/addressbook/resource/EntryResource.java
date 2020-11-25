6fa1b8634c5308fb94017d5da34cd59c8dbdec18
zc9syil5ya <chregi.sommer@gmail.com>
2020-11-25 22:05:52 +0100

    Creaate function getAllEntries()
    Function gets all entries by calling the url .../entries

--- src/main/java/ch/fhnw/swa/addressbook/resource/EntryResource.java
+++ src/main/java/ch/fhnw/swa/addressbook/resource/EntryResource.java
@@ -5,11 +5,19 @@ import ch.fhnw.swa.addressbook.model.Entry;
 import ch.fhnw.swa.addressbook.service.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class EntryResource {
     @Autowired
     private EntryService entryservice;
+    //Function gets all entries by calling the url .../entries
+    @GetMapping("/entries")
+    public List<Entry> getAllEntries() {
+        return entryservice.getAllEntries();
+    }
+
 }
