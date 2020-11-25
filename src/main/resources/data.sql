0166d06f4af29f784821c2bcc4ac29cbf84bb81c
zc9syil5ya <chregi.sommer@gmail.com>
2020-11-26 00:26:38 +0100

    format code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO TBL_ADDRESSBOOK (firstname, lastname, street, zipcode, city, email, phone) VALUES
-('Axel', 'Haar', 'Mustergasse 1a','4565','Meilen','Haar@gmail.com','0562542525'),
-('Gregor', 'Janisch', 'Pelikanweg 14','2565','Dumpfbach','Janisch@gmail.com','0562152525'),
-('Erlfried', 'König', 'Herzogsstrasse 23','2451','Heitern','Erlfried@gmail.com','0252653256');
+INSERT INTO TBL_ADDRESSBOOK (firstname, lastname, street, zipcode, city, email, phone)
+VALUES ('Axel', 'Haar', 'Mustergasse 1a', '4565', 'Meilen', 'Haar@gmail.com', '0562542525'),
+       ('Gregor', 'Janisch', 'Pelikanweg 14', '2565', 'Dumpfbach', 'Janisch@gmail.com', '0562152525'),
+       ('Erlfried', 'König', 'Herzogsstrasse 23', '2451', 'Heitern', 'Erlfried@gmail.com', '0252653256');
