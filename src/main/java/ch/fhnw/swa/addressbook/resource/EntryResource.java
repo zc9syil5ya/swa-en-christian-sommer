1a7bd5a887d409798967c661d91fe08d831d37a5
zc9syil5ya <chregi.sommer@gmail.com>
2020-11-25 21:55:37 +0100

    Update EntryResource.java
    add crossorigin and autowired EntryService

--- src/main/java/ch/fhnw/swa/addressbook/resource/EntryResource.java
+++ src/main/java/ch/fhnw/swa/addressbook/resource/EntryResource.java
@@ -1,4 +1,15 @@
 package ch.fhnw.swa.addressbook.resource;
 
+
+import ch.fhnw.swa.addressbook.model.Entry;
+import ch.fhnw.swa.addressbook.service.EntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@RestController
 public class EntryResource {
+    @Autowired
+    private EntryService entryservice;
 }
