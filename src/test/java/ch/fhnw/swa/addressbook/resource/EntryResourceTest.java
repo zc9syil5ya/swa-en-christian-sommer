ea5a34d49057bccf6eff8913c6531587ede57e0c
zc9syil5ya <chregi.sommer@gmail.com>
2020-12-03 20:28:31 +0100

    finish test Class for EntryResource REST API

--- src/test/java/ch/fhnw/swa/addressbook/resource/EntryResourceTest.java
+++ src/test/java/ch/fhnw/swa/addressbook/resource/EntryResourceTest.java
@@ -1,31 +1,38 @@
 package ch.fhnw.swa.addressbook.resource;
 
 import ch.fhnw.swa.addressbook.AddressbookApplication;
-import ch.fhnw.swa.addressbook.exception.EntryNotFoundException;
 import ch.fhnw.swa.addressbook.model.Entry;
-import ch.fhnw.swa.addressbook.service.EntryService;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Test Class for EntryResource REST API
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = AddressbookApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class EntryResourceTest {
 
     @LocalServerPort
@@ -39,7 +46,10 @@ class EntryResourceTest {
     private static final Logger logger = Logger.getLogger(EntryResource.class.getName());
 
     @Test
-    void getAllEntries() {
+    /**
+     * Test get all entry
+     */
+    void t1getAllEntries() {
         List<Entry> entries = entryresource.getAllEntries();
         logger.info(entries.toString());
         Assert.assertTrue(entries.size() == 3);
@@ -55,7 +65,10 @@ class EntryResourceTest {
     }
 
     @Test
-    void getEntry() {
+    /**
+     * Test get an entry
+     */
+    void t1getEntry() {
         Entry entry = new Entry("Paul", "Panzer", "paul@panzer.net");
         entry.setEmail("FESTERBESTERTESTER@fhnw.ch");
         HttpEntity<Entry> httpentity = new HttpEntity<>(entry);
@@ -66,7 +79,10 @@ class EntryResourceTest {
     }
 
     @Test
-    void createEntry() {
+    /**
+     * Test create an new entry
+     */
+    void t3createEntry() {
         List<Entry> entries = entryresource.getAllEntries();
         logger.info(entries.toString());
         Assert.assertTrue(entries.size() == 3);
@@ -81,7 +97,10 @@ class EntryResourceTest {
     }
 
     @Test
-    void updateEntry() {
+    /**
+     * Test update an existing entry
+     */
+    void t4updateEntry() {
         Entry entry = new Entry("Paul", "Panzer", "paul@panzer.net");
         entry.setEmail("FESTERBESTERTESTER@fhnw.ch");
         HttpEntity<Entry> httpentity = new HttpEntity<>(entry);
@@ -91,13 +110,16 @@ class EntryResourceTest {
         logger.info(response.toString());
         Assert.assertTrue(response.getStatusCode().value() == 200);
         List<Entry> entries = entryresource.getAllEntries();
-        logger.info(entries.toString());
+        logger.info(String.valueOf(entries.size()));
         Assert.assertTrue(entries.size() == 4);
         Assert.assertTrue(entries.get(3).getEmail().equals("FESTERBESTERTESTER@fhnw.ch"));
     }
 
     @Test
-    void deleteEntry() {
+    /**
+     * Test delete an entry
+     */
+    void t5deleteEntry() {
         List<Entry> entries = entryresource.getAllEntries();
         logger.info(entries.toString());
         Assert.assertTrue(entries.size() == 3);
@@ -119,9 +141,67 @@ class EntryResourceTest {
         logger.info(response.getStatusCode().toString());
         //test NOT_FOUND
         Assert.assertTrue(response.getStatusCode().value() == 404);
-
     }
 
+    @Test
+    /**
+     * Test upload
+     */
+    void t6uploadDataTest() throws IOException {
+        List<Entry> entries = entryresource.getAllEntries();
+        logger.info(entries.toString());
+        Assert.assertTrue(entries.size() == 3);
+        //test filetype png
+        MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
+        bodyMap.add("file", geMockFile("png",false));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
+        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/upload/2"),
+                HttpMethod.POST, requestEntity, String.class);
+        logger.info(response.toString());
+        Assert.assertTrue(response.getStatusCode().value() == 200);
+        //test filetype txt
+        bodyMap = new LinkedMultiValueMap<>();
+        bodyMap.add("file", geMockFile("txt",false));
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        requestEntity = new HttpEntity<>(bodyMap, headers);
+       response = restTemplate.exchange(createURLWithPort("/upload/2"),
+                HttpMethod.POST, requestEntity, String.class);
+        logger.info(response.toString());
+        Assert.assertTrue(response.getStatusCode().value() == 422);
+        //test if file to big
+        bodyMap = new LinkedMultiValueMap<>();
+        bodyMap.add("file", geMockFile("txt",true));
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        requestEntity = new HttpEntity<>(bodyMap, headers);
+        response = restTemplate.exchange(createURLWithPort("/upload/2"),
+                HttpMethod.POST, requestEntity, String.class);
+        logger.info(response.toString());
+        Assert.assertTrue(response.getStatusCode().value() == 413);
+    }
+    public static Resource geMockFile(String type, boolean oversize) throws IOException {
+        //todo maybe replace tempFile with a real file
+        Path tempFile = Files.createTempFile("test-file", "."+type);
+        if(!oversize) {
+            Files.write(tempFile, "some bytes".getBytes());
+        }else {
+            Files.write(tempFile, ("more bytes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").getBytes());
+        }
+        File file = tempFile.toFile();
+        return new FileSystemResource(file);
+    }
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
     }
